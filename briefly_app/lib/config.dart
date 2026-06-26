b7e989c5e1bf95e8cef56cdcefd808076c9f6444
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-06-26 18:10:47 +0530

    feat: pin google server client ID to debug configuration and add resource keep rules for notification icon

--- briefly_app/lib/config.dart
+++ briefly_app/lib/config.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/foundation.dart';
-
 class AppConfig {
-  static const String googleServerClientId = kDebugMode
-      ? '183248163088-mspotq609am13jo95916set808v1680l.apps.googleusercontent.com'
-      : '183248163088-d0902lpo1kvedvt71tdfivpta658p5cn.apps.googleusercontent.com';
+  static const String googleServerClientId = '183248163088-mspotq609am13jo95916set808v1680l.apps.googleusercontent.com';
 
   static const String backendUrl = String.fromEnvironment(
     'BACKEND_URL',
