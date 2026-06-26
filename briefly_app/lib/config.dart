05e19f3a270f805014e430e3822669be3e5f3f10
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-06-26 18:04:55 +0530

    refactor: switch Google server client ID based on debug or production mode

--- briefly_app/lib/config.dart
+++ briefly_app/lib/config.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/foundation.dart';
+
 class AppConfig {
-  static const String googleServerClientId = String.fromEnvironment(
-    'GOOGLE_SERVER_CLIENT_ID',
-    defaultValue: 'YOUR_GOOGLE_SERVER_CLIENT_ID.apps.googleusercontent.com',
-  );
+  static const String googleServerClientId = kDebugMode
+      ? '183248163088-mspotq609am13jo95916set808v1680l.apps.googleusercontent.com'
+      : '183248163088-d0902lpo1kvedvt71tdfivpta658p5cn.apps.googleusercontent.com';
 
   static const String backendUrl = String.fromEnvironment(
     'BACKEND_URL',
